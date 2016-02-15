4cb1db3a0db0ca68f779620d72827f7d8f6a4a0a
slremy <sekouremy@gmail.com>
2016-02-14 20:36:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo apt-get -y install python-webpy screen nginx
 wget https://raw.githubusercontent.com/slremy/bioinspiredbackend/master/nginx.conf
 wget https://raw.githubusercontent.com/slremy/bioinspiredbackend/master/websingleton.py
